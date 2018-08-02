71eb53c38f9e06da859f1518aa6b5299505d4446
Eric <biederteric.eb@gmail.com>
2018-08-02 16:45:56 +0200

    Add documentation

--- implementation/interface/src/main/java/org/codemc/worldguardwrapper/implementation/IWorldGuardImplementation.java
+++ implementation/interface/src/main/java/org/codemc/worldguardwrapper/implementation/IWorldGuardImplementation.java
@@ -10,15 +10,54 @@ import lombok.NonNull;
 
 public interface IWorldGuardImplementation {
 
+    /**
+     * Get an instance of the WorldGuardPlugin class
+     * 
+     * @return The WorldGuard plugin
+     */
     JavaPlugin getWorldGuardPlugin();
 
+    /**
+     * Get the API version of the loaded WorldGuard plugin (e.g. 6 or 7).
+     * 
+     * @return The API version
+     */
     int getApiVersion();
-    
-    Optional<Boolean> queryStateFlag(Player player, @NonNull Location location, @NonNull String flagId);
 
-    boolean registerStateFlag(@NonNull String flagId, @NonNull Boolean defaultValue);
+    /**
+     * Query a StateFlag's value for a given player at a given location.
+     * 
+     * @param player   The player
+     * @param location The location
+     * @param flag     The flag's name 
+     * @return The flag's value
+     */
+    Optional<Boolean> queryStateFlag(Player player, @NonNull Location location, @NonNull String flagName);
 
+    /**
+     * Register a {@code StateFlag}.
+     * 
+     * @param flagName     The name of the flag
+     * @param defaultValue The flag's default value
+     * @return Whether the flag has been registered
+     */
+    boolean registerStateFlag(@NonNull String flagName, @NonNull Boolean defaultValue);
+
+    /**
+     * Query a flag's value for a given player at a given location.
+     * 
+     * @param player   The player
+     * @param location The location
+     * @param flag     The flag
+     * @return The flag's value
+     */
     <T> Optional<T> queryFlag(Player player, @NonNull Location location, @NonNull AbstractFlag<T> flag);
 
+    /**
+     * Register a flag to WorldGuard's flag registry.
+     * 
+     * @param flag The flag to register
+     * @return Whether the flag has been registered
+     */
     <T> boolean registerFlag(@NonNull AbstractFlag<T> flag);
 }
